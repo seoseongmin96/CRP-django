d40896409e291bb273cfc5205c24ab84fc5bac0a
seoseongmin96 <justin090452@gmail.com>
2022-06-07 18:52:45 +0900

    ozone

--- context/domains.py
+++ context/domains.py
@@ -1,17 +1,17 @@
-#context, fname, train, test, id ,label
+# context, fname, train, test, id, label
 from dataclasses import dataclass
+from abc import *
 
 
 @dataclass
-class Dataset :
-    dname = str
-    sname : str
-    fname : str
-    train : str
-    test : str
-    id : str
-    label : str
-
+class Dataset:
+    dname: str
+    sname: str
+    fname: str
+    train: str
+    test: str
+    id: str
+    label: str
 
     @property
     def dname(self) -> str: return self._dname
@@ -54,3 +54,49 @@ class Dataset :
 
     @label.setter
     def label(self, label): self._label = label
+
+
+class PrinterBase(metaclass=ABCMeta):
+    @abstractmethod
+    def dframe(self):
+        pass
+    # new_file, csv, xls, json
+
+
+class ReaderBase(metaclass=ABCMeta):
+    @abstractmethod
+    def new_file(self):
+        pass
+
+    @abstractmethod
+    def csv(self):
+        pass
+
+    @abstractmethod
+    def xls(self):
+        pass
+
+    @abstractmethod
+    def json(self):
+        pass
+
+
+# Reader
+# Printer
+class Reader(ReaderBase):
+    def new_file(self):
+        pass
+
+    def csv(self):
+        pass
+
+    def xls(self):
+        pass
+
+    def json(self):
+        pass
+
+
+class Printer(PrinterBase):
+    def dframe(self):
+        pass
